9530d140f2c6f72c9420b53c22033dcc23d05c93
Adrien <aetaix@pixelalliance.io>
2024-10-25 15:30:55 +0200

    Add Supabase configuration and dependencies

---
+++ src/routes/(auth)/+page.svelte
@@ -0,0 +1,11 @@
+<main class="h-screen flex items-center justify-center">
+	<div class="bg-white border rounded-lg shadow w-full max-w-xl p-5 space-y-4">
+		<h1 class="text-xl font-medium">Login</h1>
+		<p>Hey! Enter your details to get sign in to your account.</p>
+		<form method="POST" action="?/login" class="flex flex-col space-y-4">
+			<input class="border rounded-lg py-2 px-3" name="email" type="email" placeholder="Email" />
+			<input class="border rounded-lg py-2 px-3" name="password" type="password" placeholder="Password" />
+			<button class="bg-green-400 text-green-950 py-2 px-3 rounded-lg">Login</button>
+		</form>
+	</div>
+</main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,23 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				green: {
+					'50': '#edfcf4',
+					'100': '#d2f9e2',
+					'200': '#a9f1ca',
+					'300': '#72e3ad',
+					'400': '#3ecf8e',
+					'500': '#16b372',
+					'600': '#0a915c',
+					'700': '#08744c',
+					'800': '#095c3e',
+					'900': '#084c34',
+					'950': '#032b1e',
+				}
+			}
+		}
 	},
 
 	plugins: [typography]
